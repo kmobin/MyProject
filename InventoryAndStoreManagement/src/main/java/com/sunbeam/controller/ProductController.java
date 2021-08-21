f72934bf91c6f5512a0515f36135da0e1896f553
D3_49365_Preet <preetjamsandekar22@gmail.com>
2021-08-21 17:25:59 +0530

    Product Controller Crossorigin Done

--- InventoryAndStoreManagement/src/main/java/com/sunbeam/controller/ProductController.java
+++ InventoryAndStoreManagement/src/main/java/com/sunbeam/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import com.sunbeam.model.ProductModel;
 import com.sunbeam.services.ProductService;
 import com.sunbeam.servicesImpl.ProductServiceImpl;
 
+@CrossOrigin
 @RequestMapping("/product")
 @RestController
 public class ProductController {
